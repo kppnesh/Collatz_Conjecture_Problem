a9e61016b43dac6d04debb577880652e76b51f77
Pop <kppnesh@gmail.com>
2021-08-26 13:20:05 +0530

    Performance improvement and comments are added to improve readability

--- MathProj/Calculation.cs
+++ MathProj/Calculation.cs
@@ -3,98 +3,157 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 using System.IO;
+using System.Threading;
 
 namespace MathProj
 {
     class Calculation
     {
+        #region Local variables
         BigInteger x;
+        BigInteger One = BigInteger.One;
+        BigInteger MinusOne = BigInteger.MinusOne;
+        BigInteger Two = BigInteger.Parse("2");
+        BigInteger Three = BigInteger.Parse("3");
+        BigInteger checkPoint = BigInteger.Parse("100000");
+        BigInteger Zero = BigInteger.Zero;
+        bool flag = true;
+
+        //Not Working: Using array to store values and skip those values//
+        //HashSet<BigInteger> hashSet =new HashSet<BigInteger>();
+        //SortedSet<BigInteger> hashSet = new SortedSet<BigInteger>();
+        #endregion
+
+        #region Constructor used to initialize seed value
         public Calculation(BigInteger startValue)
         {
-            //Constructor used to initialize the start value
-            x=startValue;
+            x = startValue;
             this.calculate();
         }
+        #endregion
 
-        //Iterate through numbers till we find our desired output
+        #region Logic for the problem
+        //Calculate method will increment seed number till we find our desired number
         public void calculate()
         {
             try
             {
                 BigInteger temp=x;
+                Thread a = new Thread(CurrentExecution);
+                a.Start();
                 do
                 {
-                    temp=Logic(temp);
-                    if (BigInteger.Compare(temp, BigInteger.Zero) == 0)
+                    temp = Logic(temp);
+                    //checks output form oddeven method is '0'                
+                    if (BigInteger.Compare(temp, Zero) == 0)        
                     {
-                       temp= x = BigInteger.Add(x, BigInteger.Parse("2"));
-                        if (BigInteger.Compare(BigInteger.Remainder(x, BigInteger.Parse("100000")), 1) == 0)
-                        {
-                            //Console.Clear();
-                            Console.WriteLine("Current no: {0}", x);
-                        }
+                       //increments seed number by 2
+                       temp = x = BigInteger.Add(x, Two);     
+                        //while (hashSet.Remove(temp))
+                        //{
+                        //    //check number present in array and skip
+                        //    temp = x = BigInteger.Add(x, Two);      //incrementing with 2 to get next odd number
+                        //}
+
+                        //Priniting current number in the console(now moved to new thread)
+                        //if (BigInteger.Compare(BigInteger.Remainder(x, checkPoint), 1) == 0)
+                        //{
+                        //    //Console.Clear();
+                        //    Console.WriteLine("Current no: {0}", x);            
+                        //}
                     }
-                    else if (BigInteger.Compare(temp, BigInteger.One) == 0)
+                    //checks output form oddeven method is '1'
+                    else if (BigInteger.Compare(temp, One) == 0)     
                     {
+                        flag = false;
                         Console.WriteLine("The answer is {0}", x);
                         return;
                     }
-                    else if (BigInteger.Compare(temp, BigInteger.MinusOne)==0)
+                    //checks output form oddeven method is '-1'
+                    else if (BigInteger.Compare(temp, MinusOne)==0)  
                     {
+                        flag = false;
                         Console.WriteLine("some error occured in Logic class");
                         return;
                     }
-                } while (BigInteger.Compare(temp,BigInteger.Zero)!=0);
+                } while (true);
             }
             catch (Exception e)
             {
+                flag = false;
                 Console.WriteLine(e.Message);
             }
         }
 
-        //Core problem logic is written here
+        //Core logic is written here
         BigInteger Logic(BigInteger n)
         {
             try
             {
                 BigInteger temp = oddEven(n);
-                if (BigInteger.Compare(temp, x) == 0)
+                if (BigInteger.Remainder(temp, Two) != 0)
                 {
-                    return BigInteger.One;
+                    //checks current number is same as seed number
+                    if (BigInteger.Compare(temp, x) == 0)           
+                    {
+                        return One;
+                    }
+                    else
+                    {
+                        //hashSet.Add(temp);   //stores new number in array
+                        return temp;
+                    }
                 }
-                else if (BigInteger.Compare(temp, BigInteger.Multiply(BigInteger.Parse("2"), x)) < 0)
+                //checks current even number < 2 times the seed number
+                else if (BigInteger.Compare(temp, BigInteger.Multiply(Two, x)) < 0)       
                 {
-                    return BigInteger.Zero;
+                    return Zero;
                 }
                 else { return temp; }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                return BigInteger.MinusOne;
+                return MinusOne;
             }
         }
 
-        //Method to find a number is odd/even and proceed calculations 
+        //Do calculations based on odd/even number 
         BigInteger oddEven(BigInteger n)
         {
             try
             {
-                if (BigInteger.Remainder(n, BigInteger.Parse("2")) ==0)
+                if (BigInteger.Remainder(n, Two) ==0)
                 {
-                    return BigInteger.Divide(n, BigInteger.Parse("2"));
+                    return BigInteger.Divide(n, Two);             //Line is executed if number is even
                 }
                 else
                 {
-                    return BigInteger.Divide(BigInteger.Add(BigInteger.Multiply(BigInteger.Parse("3"), n), BigInteger.One), BigInteger.Parse("2"));
+                    return BigInteger.Divide(BigInteger.Add(BigInteger.Multiply(Three, n), One), Two);     //line is executed if number is odd
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                return 0;
+                return MinusOne;
             }
         }
+        #endregion
 
+        #region Prints current executed seed number
+        void CurrentExecution()
+        {
+            do
+            {
+                //if capslocak is turned on, clears screen
+                if (Console.CapsLock) 
+                { 
+                    Console.Clear();
+                }
+                Console.WriteLine("current no: {0}", x);
+            }
+            while (flag);
+        }
+        #endregion
     }
 }
--- MathProj/Program.cs
+++ MathProj/Program.cs
@@ -8,8 +8,14 @@ namespace MathProj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Calculation cal1= new Calculation(BigInteger.Parse(Console.ReadLine()));
-            //cal1.calculate(1);
+            string input;
+            //accepts only positive odd numbers
+            do
+            {
+                Console.WriteLine("Please enter a value to start(Positive odd numbers only):");       
+                input = Console.ReadLine();
+            } while (BigInteger.Remainder(BigInteger.Parse(input), BigInteger.Parse("2")) == 0 || BigInteger.Parse(input)<BigInteger.Zero);
+            Calculation cal1= new Calculation(BigInteger.Parse(input));
         }
     }
 }
